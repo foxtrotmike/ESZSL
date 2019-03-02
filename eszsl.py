ca60289d6ae02bd9ca7fd8d629daa22db7d7447c
Fayyaz ul Amir Afsar Minhas <foxtrotmike@users.noreply.github.com>
2019-03-02 10:14:05 +0500

    Update eszsl.py
    Binary classification tested.

--- eszsl.py
+++ eszsl.py
@@ -4,40 +4,11 @@
 @author-email: afsar at pieas dot edu dot pk
 Implementation of embarrasingly simple zero shot learning
 """
-from scipy import spatial
-from numpy.random import randn,randint #importing randn
-
+from numpy.random import randn #importing randn
 import numpy as np #importing numpy
 import matplotlib.pyplot as plt #importing plotting module
 import itertools
 import warnings
-from scipy.spatial import Voronoi, voronoi_plot_2d
-from scipy.stats import kde
-def plotDensity_2d(X,Y):
-    nbins = 200
-    minx, maxx = np.min(X[:,0]), np.max(X[:,0])
-    miny, maxy = np.min(X[:,1]), np.max(X[:,1])
-    xi, yi = np.mgrid[minx:maxx:nbins*1j, miny:maxy:nbins*1j]
-    def calcDensity(xx):
-        k = kde.gaussian_kde(xx.T)        
-        zi = k(np.vstack([xi.flatten(), yi.flatten()]))
-        return zi.reshape(xi.shape)
-    
-    pz=calcDensity(X[Y==1,:2])
-    nz=calcDensity(X[Y==-1,:2])
-    
-    c1=plt.contour(xi, yi, pz,cmap=plt.cm.Greys_r,levels=np.percentile(pz,[75,90,95,97,99])); plt.clabel(c1, inline=1)
-    c2=plt.contour(xi, yi, nz,cmap=plt.cm.Purples_r,levels=np.percentile(nz,[75,90,95,97,99])); plt.clabel(c2, inline=1)
-    plt.pcolormesh(xi, yi, 1-pz*nz,cmap=plt.cm.Blues,vmax=1,vmin=0.99);plt.colorbar()
-    markers = ('s','o')
-    plt.scatter(X[Y==1,0],X[Y==1,1],marker = markers[0], c = 'y', s = 30)
-    plt.scatter(X[Y==-1,0],X[Y==-1,1],marker = markers[1],c = 'c', s = 30)
-    plt.xlabel('$x_1$')
-    plt.ylabel('$x_2$')   
-    #
-    plt.grid()
-    plt.show()
-                   
 
 def plotit(X,Y=None,clf=None, markers = ('s','o'), hold = False, transform = None):
     """
@@ -93,83 +64,42 @@ def getExamples(n=100,d=2):
     """
     Xp = randn(n,d)#+1   #generate n examples of the positie class
     Xp=Xp
-    Xn = randn(n,d)#-1   #generate n examples of the negative class
+    Xn = randn(int(n/2),d)#-1   #generate n examples of the negative class
     Xn=Xn-5
+    Xn2 = randn(int(n/2),d)#-1   #generate n examples of the negative class
+    Xn2=Xn2+5
+    Xn = np.vstack((Xn,Xn2))
     X = np.vstack((Xp,Xn))  #Stack the examples together to a single matrix
-    Y = np.array([+1]*n+[-1]*n) #Associate Labels
+    Y = np.array([+1]*n+[-1]*int(n)) #Associate Labels
     return (X,Y) 
 
 
+def poly(X1,X2,**kwargs):
+    if 'degree' not in kwargs:
+        d = 1
+    else:
+        d = kwargs['degree']
         
+    return (np.dot(X1,X2.T)+1)**d
 
-class ESZSLK:
+class ESZSL:
     """
-    Implementatio of Kernelized Zero Shot Learning
+    Implementation of Generalized Zero Shot Learning
     Author: Dr. Fayyaz Minhas, DCIS, PIEAS
     It implements the paper by Bernardino Romera-Paredes and Philip H. S. Torr
     An embarrassingly simple approach to zero-shot learning, (ICML 2015).
     No warranties. Under Creative Commons License.
     """
-    def __init__(self, lambdap = 0.1, sigmap = 0.1):
+    def __init__(self, lambdap = 0.1, sigmap = 0.1, kernel = poly, **kwargs):
         """
         lambdap: Regularization parameter for kernel/feature space
         sigmap: Regularization parameter for Attribute Space
+        kernel: kernel function (default is poly)
+        kwargs: optional, any kernel arguments
         """
         self.sigmap = sigmap
         self.lambdap = lambdap
         self.A = None
-        
-    def fit(self,K,Y,S = None):
-        """
-        Training:
-            K: (mxm) kernel matrix (m is the number of triaining examples)
-            Y: (mxz) label matrix (z is the number of classes)
-            S: (zxa) attribute matrix for all classes (a is the number of attributes)
-                Optional: Default, S = I
-        It computes mxa sized $$A=(K^TK+\lambda I)^{-1}KYS(S^TS+\sigma I)^{-1}$$
-        """
-        if S is None:
-            S = np.eye(Y.shape[1])
-        KK = np.dot(K.T,K)
-        KK = np.linalg.inv(KK+self.lambdap*np.eye(KK.shape[0]))
-        KYS = np.dot(np.dot(K,Y),S)    
-        SS = np.dot(S.T,S)
-        SS = np.linalg.inv(SS+self.sigmap*np.eye(SS.shape[0]))
-        self.A = np.dot(np.dot(KK,KYS),SS)
-
-    def decision_function(self,K,S = None):
-        """
-        Testing:
-            K: (m'xm) kernel matrix (m' is the number of test examples)
-            S: (z'xa) attribute matrix (z' is the number of test classes)
-        It implements: $$Z = SA^TK^T$$ to generate class scores for each test example 
-        Returns:
-            Z: (m'xz') matrix of class scores
-        """
-        if S is None:
-            S = np.eye(Y.shape[1])
-        Z = np.dot(np.dot(S,self.A.T),K.T).T
-        return Z[:,1]
-    
-def linearKernel(X1,X2,**kwargs):
-    return (np.dot(X1,X2.T)+1)**3
-
-class ESZSL:
-    """
-    Implementatio of Generalized Zero Shot Learning
-    Author: Dr. Fayyaz Minhas, DCIS, PIEAS
-    It implements the paper by Bernardino Romera-Paredes and Philip H. S. Torr
-    An embarrassingly simple approach to zero-shot learning, (ICML 2015).
-    No warranties. Under Creative Commons License.
-    """
-    def __init__(self, lambdap = 0.1, sigmap = 0.1, kernel = linearKernel, **kwargs):
-        """
-        lambdap: Regularization parameter for kernel/feature space
-        sigmap: Regularization parameter for Attribute Space
-        kernel: kernel function (default is linear kernel)
-        kwargs: optional, any kernel arguments
-        """
-        self.clf = ESZSLK(lambdap,sigmap)
         self.kwargs = kwargs
         self.kernel = kernel
     def fit(self,X,Y,S = None):
@@ -179,27 +109,50 @@ class ESZSL:
             Y: (mxz) label matrix (z is the number of classes)
             S: (zxa) attribute matrix for all classes (a is the number of attributes)
                 Optional: Default, S = I
+        Training: (when self.kernel = "precomputed")
+            X = K: (mxm) kernel matrix (m is the number of triaining examples)
+        It computes mxa sized $$A=(K^TK+\lambda I)^{-1}KYS(S^TS+\sigma I)^{-1}$$
         
         """
-        self.X = X
-        K = self.kernel(X,X,**self.kwargs) 
-        self.clf.fit(K,Y,S)
+        if S is None:
+            S = np.eye(Y.shape[1])       
+        if self.kernel=='precomputed':
+            K = X
+        else:            
+            self.X = X
+            K = self.kernel(X,X,**self.kwargs) 
+        KK = np.dot(K.T,K)
+        KK = np.linalg.inv(KK+self.lambdap*np.eye(KK.shape[0]))
+        KYS = np.dot(np.dot(K,Y),S)    
+        SS = np.dot(S.T,S)
+        SS = np.linalg.inv(SS+self.sigmap*np.eye(SS.shape[0]))
+        self.A = np.dot(np.dot(KK,KYS),SS)
+        
     def decision_function(self,X,S = None):
         """
         Testing:
             X: (m'xd) kernel matrix (m' is the number of test examples)
-            S: (z'xa) attribute matrix (z' is the number of test classes)
-        
+            S: (z'xa) attribute matrix (z' is the number of test classes)        
+       
+        Testing: (when self.kernel = "precomputed")
+            X = K: (m'xm) kernel matrix (m' is the number of test examples)            
+        It implements: $$Z = SA^TK^T$$ to generate class scores for each test example 
         Returns:
             Z: (m'xz') matrix of class scores
+        
         """
-        K = self.kernel(X,self.X,**self.kwargs)
-        Z = self.clf.decision_function(K,S)
+        if S is None:
+            S = np.eye(clf.A.shape[1])[-1,:]
+        if self.kernel=='precomputed':
+            K = X
+        else:
+            K = self.kernel(X,self.X,**self.kwargs)
+        Z = np.dot(np.dot(S,self.A.T),K.T).T
         return Z
         
 if __name__ == '__main__':
     plt.close("all")    
-    #%% Data Generation and Density Plotting
+    #%% Data Generation
     n = 500 #number of examples of each class
     d = 2 #number of dimensions
     Xtr,Ytr = getExamples(n=n,d=d) #Generate Training Examples    
@@ -207,28 +160,25 @@ if __name__ == '__main__':
     print("Number of negative examples in training: ", np.sum(Ytr==-1))
     print("Dimensions of the data: ", Xtr.shape[1])   
     Xtt,Ytt = getExamples(n=100,d=d) #Generate Testing Examples 
-    
+    #%% Setting up classlabel matrix Y and attribute matrix S for binary classification
     Y = -1*np.ones((Xtr.shape[0],2))
     for i,v in enumerate(np.array((Ytr+1)/2,dtype=np.int)):
         Y[i,v]=1
     S = np.eye(2)        
-    clf = ESZSL(sigmap = 1, lambdap = 1)
+    #%% Training and evaluation, plotting
+    clf = ESZSL(sigmap = 0.1, lambdap = 1.0, kernel = poly, degree = 3)
     clf.fit(Xtr,Y,S)
-    Z = clf.decision_function(Xtr,S)
+    Z = clf.decision_function(Xtr,S)[:,1]
     print("Train accuracy",accuracy(Ytr,2*(Z>0)-1))
-    Z = clf.decision_function(Xtt,S)
-    print("Train accuracy",accuracy(Ytt,2*(Z>0)-1))
-    plotit(Xtr,Ytr,clf=clf.decision_function)
-    1/0
-    
-    K = np.dot(Xtr,Xtr.T)        
-    
-
-        
-    clf = ESZSLK(sigmap = 0.1, lambdap = 0.1)
+    Z = clf.decision_function(Xtt,S)[:,1]
+    print("Test accuracy",accuracy(Ytt,2*(Z>0)-1))
+    plotit(Xtr,Ytr,clf=clf.decision_function)    
+    #%% Training and evaluation for precomputed matrix
+    K = (np.dot(Xtr,Xtr.T)+1)**2
+    clf = ESZSL(sigmap = 0.1, lambdap = 0.1, kernel = 'precomputed')
     clf.fit(K,Y,S)
-    Z = clf.decision_function(K,S)
-    print("Train accuracy",accuracy(Ytr,2*(Z[:,1]>0)-1))
-    Z = clf.decision_function(np.dot(Xtt,Xtr.T),S)
-    print("Test accuracy",accuracy(Ytt,2*(Z[:,1]>0)-1))
+    Z = clf.decision_function(K,S)[:,1]
+    print("Train accuracy",accuracy(Ytr,2*(Z>0)-1))
+    Z = clf.decision_function((np.dot(Xtt,Xtr.T)+1)**2,S)[:,1]
+    print("Test accuracy",accuracy(Ytt,2*(Z>0)-1))
     
