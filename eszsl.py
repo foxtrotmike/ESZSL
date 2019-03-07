c44a418087c3db53a1f61a8d5fcd573f8a6c69ff
Fayyaz ul Amir Afsar Minhas <foxtrotmike@users.noreply.github.com>
2019-03-07 16:15:18 +0500

    Add files via upload
    Verified to produce the same outputs as the original code for the sun data

--- eszsl.py
+++ eszsl.py
@@ -4,25 +4,51 @@
 @author-email: afsar at pieas dot edu dot pk
 Implementation of embarrasingly simple zero shot learning
 """
+from __future__ import print_function
 from numpy.random import randn #importing randn
 import numpy as np #importing numpy
 import matplotlib.pyplot as plt #importing plotting module
 import itertools
 import warnings
 
-def plotit(X,Y=None,clf=None, markers = ('.','o'), hold = False, transform = None,**kwargs):
+def plotit(X,Y=None,clf=None,  conts = None, ccolors = ('b','k','r'), colors = ('c','y'), markers = ('s','o'), hold = False, transform = None,**kwargs):
     """
-    Just a function for showing a data scatter plot and classification boundary
-    of a classifier clf
+    A function for showing data scatter plot and classification boundary
+    of a classifier for 2D data
+        X: nxd  matrix of data points
+        Y: (optional) n vector of class labels
+        clf: (optional) classification/discriminant function handle
+        conts: (optional) contours (if None, contours are drawn for each class boundary)
+        ccolors: (optional) colors for contours   
+        colors: (optional) colors for each class (sorted wrt class id)
+            can be 'scaled' or 'random' or a list/tuple of color ids
+        markers: (optional) markers for each class (sorted wrt class id)
+        hold: Whether to hold the plot or not for overlay (default: False).
+        transform: (optional) a function handle for transforming data before passing to clf
+        kwargs: any keyword arguments to be passed to clf (if any)        
     """
     if clf is not None and X.shape[1]!=2:
         warnings.warn("Data Dimensionality is not 2. Unable to plot.")
         return
-    
+    if markers is None:
+        markers = ('.',)
+        
+    d0,d1 = (0,1)
+    minx, maxx = np.min(X[:,d0]), np.max(X[:,d0])
+    miny, maxy = np.min(X[:,d1]), np.max(X[:,d1])
     eps=1e-6
-    minx, maxx = np.min(X[:,0]), np.max(X[:,0])
-    miny, maxy = np.min(X[:,1]), np.max(X[:,1])
+
     
+    if Y is not None:
+        classes = sorted(set(Y))
+        if conts is None:
+            conts = list(classes)        
+        vmin,vmax = classes[0]-eps,classes[-1]+eps
+    else:
+        vmin,vmax=-2-eps,2+eps
+        if conts is None:            
+            conts = sorted([-1+eps,0,1-eps])
+        
     if clf is not None:
         npts = 150
         x = np.linspace(minx,maxx,npts)
@@ -34,27 +60,33 @@ def plotit(X,Y=None,clf=None, markers = ('.','o'), hold = False, transform = Non
         
         z = np.reshape(z,(npts,npts)).T        
         extent = [minx,maxx,miny,maxy]
-        plt.contour(x,y,z,[-1+eps,0,1-eps],linewidths = [2],colors=('b','k','r'),extent=extent, label='f(x)=0')
+        
+        plt.contour(x,y,z,conts,linewidths = [2],colors=ccolors,extent=extent, label='f(x)=0')
         #plt.imshow(np.flipud(z), extent = extent, cmap=plt.cm.Purples, vmin = -2, vmax = +2); plt.colorbar()
-        plt.pcolormesh(x, y, z,cmap=plt.cm.Purples,vmin=-2,vmax=+2);plt.colorbar()
+        plt.pcolormesh(x, y, z,cmap=plt.cm.Purples,vmin=vmin,vmax=vmax);plt.colorbar()
         plt.axis([minx,maxx,miny,maxy])
     
-    if Y is not None:
-        for y in set(Y):
-            plt.scatter(X[Y==y,0],X[Y==y,1], marker = markers[0],c = np.random.rand(1,3), s = 30)
-        #plt.scatter(X[Y==-1,0],X[Y==-1,1],marker = markers[1],c = 'c', s = 30)
-        plt.xlabel('$x_1$')
-        plt.ylabel('$x_2$')        
+    if Y is not None:        
+        for i,y in enumerate(classes):
+            if colors is None or colors=='scaled':
+                cc = np.array([[i,i,i]])/float(len(classes))
+            elif colors =='random':
+                cc = np.array([[np.random.rand(),np.random.rand(),np.random.rand()]])
+            else:
+                cc = colors[i%len(colors)]
+            mm = markers[i%len(markers)]
+            plt.scatter(X[Y==y,d0],X[Y==y,d1], marker = mm,c = cc, s = 30)     
          
     else:
-        plt.scatter(X[:,0],X[:,1],marker = '.', c = 'k', s = 5)
+        plt.scatter(X[:,d0],X[:,d1],marker = markers[0], c = 'k', s = 5)
+    plt.xlabel('$x_1$')
+    plt.ylabel('$x_2$')   
     if not hold:
-        plt.grid()
-        
+        plt.grid()        
         plt.show()
     
 def accuracy(ytarget,ypredicted):
-    return np.sum(ytarget == ypredicted)/len(ytarget)
+    return np.mean(ytarget == ypredicted)
 
 
 def getExamples(n=100,d=2):
@@ -71,7 +103,7 @@ def getExamples(n=100,d=2):
     Xn2=Xn2+5
     Xn = np.vstack((Xn,Xn2))
     X = np.vstack((Xp,Xn))  #Stack the examples together to a single matrix
-    Y = np.array([+1]*n+[-1]*int(n)) #Associate Labels
+    Y = np.array([0]*n+[1]*int(n/2)+[2]*int(n/2)) #Associate Labels
     return (X,Y) 
 
 
@@ -103,8 +135,13 @@ class ESZSL:
         self.A = None
         self.kwargs = kwargs
         self.kernel = kernel
+        
     def fit(self,X,Y,S = None):
-        """
+        """    X,Y = getExamples()
+    clf = lambda x: 2*np.sum(x,axis=1)-2.5 #dummy classifier
+    plotit(X = X, Y = Y, clf = clf, conts =[-1,0,1], colors = 'random')
+    1/0
+    plt.close("all")    
         Training:
             X: (mxd) kernel matrix (m is the number of triaining examples,d is feature dims)
             Y: (mxz) label matrix (z is the number of classes)
@@ -116,14 +153,15 @@ class ESZSL:
         
         """
         if S is None:
-            S = np.eye(Y.shape[1])       
+            S = np.eye(Y.shape[1])
+
         if self.kernel=='precomputed':
             K = X
         else:            
             self.X = X
             K = self.kernel(X,X,**self.kwargs) 
         KK = np.dot(K.T,K)
-        KK = np.linalg.inv(KK+self.lambdap*np.eye(KK.shape[0]))
+        KK = np.linalg.inv(KK+self.lambdap*(np.eye(K.shape[0])))
         KYS = np.dot(np.dot(K,Y),S)    
         SS = np.dot(S.T,S)
         SS = np.linalg.inv(SS+self.sigmap*np.eye(SS.shape[0]))
@@ -144,55 +182,59 @@ class ESZSL:
         """
         if S is None:
             S = np.eye(clf.A.shape[1])[0,:]
+
         if self.kernel=='precomputed':
             K = X
         else:
             K = self.kernel(X,self.X,**self.kwargs)
         Z = np.dot(np.dot(S,self.A.T),K.T).T
         return Z
-    
+    def predict(self,X,S=None):
+        return np.argmax(self.decision_function(X,S),axis=1)
         
 if __name__ == '__main__':
-    plt.close("all")    
-    #%% Data Generation
-    n = 500 #number of examples of each class
-    d = 2 #number of dimensions
-    Xtr,Ytr = getExamples(n=n,d=d) #Generate Training Examples    
-    print("Number of positive examples in training: ", np.sum(Ytr==1))
-    print("Number of negative examples in training: ", np.sum(Ytr==-1))
-    print("Dimensions of the data: ", Xtr.shape[1])   
-    Xtt,Ytt = getExamples(n=100,d=d) #Generate Testing Examples 
-    #%% Setting up classlabel matrix Y and attribute matrix S for binary classification
-    Y = -1*np.ones((Xtr.shape[0],2))
-    for i,v in enumerate(np.array((Ytr+1)/2,dtype=np.int)):
-        Y[i,v]=1
-    S = 5*np.eye(2,2)
-    
-    
-         
-    #%% Training and evaluation, plotting
-    clf = ESZSL(sigmap = 0.1, lambdap = 1.0, kernel = poly, degree = 2)
-    clf.fit(Xtr,Y,S)
-    Z = clf.decision_function(Xtr,S)[:,1]
-    print("Train accuracy",accuracy(Ytr,2*(Z>0)-1))
-    Z = clf.decision_function(Xtt,S)[:,1]
-    print("Test accuracy",accuracy(Ytt,2*(Z>0)-1))
-    plotit(Xtr,Ytr,clf=clf.decision_function)    
-#%% Training and evaluation for precomputed matrix
-    K = (np.dot(Xtr,Xtr.T)+1)**2
-    clf = ESZSL(sigmap = 0.1, lambdap = 0.1, kernel = 'precomputed')
-    clf.fit(K,Y,S)
-    Z = clf.decision_function(K,S)[:,1]
-    print("Train accuracy",accuracy(Ytr,2*(Z>0)-1))
-    Z = clf.decision_function((np.dot(Xtt,Xtr.T)+1)**2,S)[:,1]
-    print("Test accuracy",accuracy(Ytt,2*(Z>0)-1))
+#%% Data Generation for simple classification 
+
+#    n = 500 #number of examples of each class
+#    d = 2 #number of dimensions
+#    Xtr,Ytr = getExamples(n=n,d=d) #Generate Training Examples    
+#    print("Number of positive examples in training: ", np.sum(Ytr==1))
+#    print("Number of negative examples in training: ", np.sum(Ytr==-1))
+#    print("Dimensions of the data: ", Xtr.shape[1])   
+#    Xtt,Ytt = getExamples(n=100,d=d) #Generate Testing Examples 
+#    z  = len(set(Ytt))
+#    #%% Setting up classlabel matrix Y and attribute matrix S for binary classification
+#    Y = -1*np.ones((Xtr.shape[0],z))
+#    for i in range(len(Y)):
+#        Y[i,Ytr[i]]=1
+#    S = np.eye(z,z)
+#    
+#    
+#         
+#    #%% Training and evaluation, plotting
+#    clf = ESZSL(sigmap = 0.1, lambdap = 0.05, kernel = poly, degree = 1)
+#    clf.fit(Xtr,Y,S)
+#    Z = clf.decision_function(Xtr,S)[:,1]
+#    print("Train accuracy",accuracy(Ytr,clf.predict(Xtr,S)))
+#    Z = clf.decision_function(Xtt,S)[:,1]
+#    print("Train accuracy",accuracy(Ytt,clf.predict(Xtt,S)))
+#    plotit(Xtr,Ytr,clf=clf.predict,S=S)    
+#    1/0
+##%% Training and evaluation for precomputed matrix
+#    K = (np.dot(Xtr,Xtr.T)+1)**2
+#    clf = ESZSL(sigmap = 0.1, lambdap = 0.1, kernel = 'precomputed')
+#    clf.fit(K,Y,S)
+#    Z = clf.decision_function(K,S)[:,1]
+#    print("Train accuracy",accuracy(Ytr,2*(Z>0)-1))
+#    Z = clf.decision_function((np.dot(Xtt,Xtr.T)+1)**2,S)[:,1]
+#    print("Test accuracy",accuracy(Ytt,2*(Z>0)-1))
 #    
 #%% Generalized zero shot learning
     class dataGenerator:
         def __init__(self,a=100,d=2):
             self.a = a
             self.d = d
-            self.V = np.random.randn(self.a,self.d)
+            self.V = 1*np.random.randn(self.a,self.d)
         def getAttributes(self,z = 3):
             return np.random.binomial(1,0.5,(z,self.a))        
         def getData(self,S, n = 50, useOldNormalization = False):
@@ -203,7 +245,7 @@ if __name__ == '__main__':
             X = []
             Y = -1*np.ones((n*z,z))
             for i in range(z):
-                X.append(np.random.randn(n,d)+C[i,:])
+                X.append(0.5*np.random.randn(n,d)+C[i,:])
                 Y[i*n:(i+1)*n,i]=1.0
             X = np.vstack(X)
             if not useOldNormalization:
@@ -217,21 +259,33 @@ if __name__ == '__main__':
     
     
     a = 100
-    d = 10
+    d = 2
+    z = 8
+    zd = 2
+    sigmap = 0.1
+    lambdap = 0.1
+    
     dgen = dataGenerator(a,d)
-    S = dgen.getAttributes(z=100)
-    X,Y = dgen.getData(S, n = 50)
-    clf = ESZSL(sigmap = 1e-1, lambdap = 1e1, kernel = poly, degree = 1)
+    S = dgen.getAttributes(z=z)
+    X,Y = dgen.getData(S, n = 100)
+    yc = np.argmax(Y,axis=1)
+    #    X2, Y2, S2 = X[yc>=z-zd,:], Y[yc>=z-zd,(z-zd):], S[(z-zd):]
+    #    X, Y, S = X[yc<(z-zd),:], Y[yc<z-zd,:(z-zd)], S[:(z-zd)]
+    clf = ESZSL(sigmap = sigmap, lambdap =lambdap, kernel = poly, degree = 3)
     clf.fit(X,Y,S)
     Z = clf.decision_function(X,S)
-    print("Train Accuracy",MCAccuracy(Y,Z))
+#                print("Train Accuracy",MCAccuracy(Y,Z))
     S1 = S
     X1,Y1 = dgen.getData(S1, n = 50, useOldNormalization = True)
     Z1 = clf.decision_function(X1,S1)
     print("Test Accuracy",MCAccuracy(Y1,Z1))
-    S2 = dgen.getAttributes(z=3)
+    S2 = dgen.getAttributes(z=zd)#S+(2*(np.random.rand(*S.shape)>0.5)-1)*0.2#
     X2,Y2 = dgen.getData(S2, n = 50, useOldNormalization = True)
     Z2 = clf.decision_function(X2,S2)
-    print("ZSL Accuracy",MCAccuracy(Y2,Z2))
+    zsla = MCAccuracy(Y2,Z2)
+    print("ZSL Accuracy",zsla)
+
     plt.close('all')
-    plotit(X,np.argmax(Y,axis=1));plotit(X2,np.argmax(Y2,axis=1),markers=('^','o'))
+    plotit(X,np.argmax(Y,axis=1),clf=clf.predict,S=S, colors = 'random', ccolors = None, markers = None);
+    plt.figure()
+    plotit(X2,np.argmax(Y2,axis=1),clf=clf.predict,S=S2)
